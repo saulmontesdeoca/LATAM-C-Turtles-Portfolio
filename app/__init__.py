76808d6a0cf6fdba0555a6e1b5e0f57008af9541
Cloud User <centos@ip-172-31-60-229.ec2.internal>
2021-07-02 17:12:52 +0000

    Docker-compose with nginx

--- app/__init__.py
+++ app/__init__.py
@@ -57,29 +57,23 @@ def register():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
         error = None
 
         if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
+        elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
         if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
+            new_user = UserModel(username, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
             return f"User {username} created successfully"
         else:
             return error, 418
 
-    ## TODO: Return a restister page
     return render_template('register.html')
 
 @app.route('/login', methods=('GET', 'POST'))
@@ -87,21 +81,16 @@ def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
         error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+        user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
             error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
+        elif not check_password_hash(user.password, password):
             error = 'Incorrect password.'
 
         if error is None:
-            return "Login Successful", 200 
+            return "Login Successful", 200
         else:
-            return render_template('login.html', error=error), 418
-    
-    ## TODO: Return a login page
+            return error, 418
     return render_template('login.html', error='')
