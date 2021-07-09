852360085c2497cd787a15751b7ac76b5f57bbf8
Saul Montes De Oca <mr.sauce97@gmail.com>
2021-07-09 01:01:54 -0500

    🕹 Added links

--- app/static/js/profile.js
+++ app/static/js/profile.js
@@ -4,4 +4,5 @@ ScrollReveal().reveal('.name-title', { delay: 1800 });
 ScrollReveal().reveal('.breaker', { delay: 1000 });
 ScrollReveal().reveal('.name-subtitle', { delay: 2800 });
 ScrollReveal().reveal('.description', { delay: 2800 });
+ScrollReveal().reveal('.links', { delay: 3300 });
 ScrollReveal().reveal('.curriculum', { delay: 3800 });
